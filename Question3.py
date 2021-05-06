2f6748be1a4c7bce7be6a1c07ea4f05a53900d67
Asish <asishpatnaik2000@gmail.com>
2021-05-06 13:52:23 +0530

    Assignment 1

---
+++ Question3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import math
+def volume(n):
+    return 4/3*math.pi*n*n
+
+
+# In[4]:
+
+
+print("Volume: ",volume(12),"cm\u00B3")
+
+
+# In[ ]:
+
+
+
+
